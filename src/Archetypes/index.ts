4521c389162daeb45de98813624176f1ae52d6cc
kerley <kerley.kls@gmail.com>
2022-08-09 22:14:14 -0300

    feat: just test a new push

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -5,4 +5,4 @@ import Ranger from './Ranger';
 import Warrior from './Warrior';
 
 export default Archetype;
-export { Mage, Necromancer, Ranger, Warrior };
+export { Necromancer, Mage, Ranger, Warrior };
